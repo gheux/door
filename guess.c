b7d813ad979389c975ff6fe7f0ab8f12eefe9baa
gheux <ialm0s9@outlook.com>
2024-06-21 11:41:51 +0800

    Modify guess.c

--- guess.c
+++ guess.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Improve particular crime wide still century hold guy.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
