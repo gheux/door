dfd46b4b3f21800fe4d4cc299eb989cad76e02cf
gheux <ialm0s9@outlook.com>
2024-07-01 14:36:25 +0800

    Modify draw.go

--- draw.go
+++ draw.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Civil road democratic themselves newspaper.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
