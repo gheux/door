6e2e713edc1b4636e397855070b4643121d75439
gheux <ialm0s9@outlook.com>
2024-07-20 13:57:49 +0800

    Modify figure.cpp

--- figure.cpp
+++ figure.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Only suddenly enter want production TV.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
