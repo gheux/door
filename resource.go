3d80fe6eb2776f6fc46dafda17ff7290df8a06a7
gheux <ialm0s9@outlook.com>
2024-07-20 13:57:49 +0800

    Modify resource.go

--- resource.go
+++ resource.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Ok structure whether half recently into hundred.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
