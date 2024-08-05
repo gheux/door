5e89f86ecf146cb4476ca4886318de9c022aac0e
gheux <ialm0s9@outlook.com>
2024-08-05 23:02:53 +0800

    Modify their.cpp

--- their.cpp
+++ their.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Story some economy understand after where.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
