78db9bc31b718289c75387cef69a8c59d5691fbb
gheux <ialm0s9@outlook.com>
2024-08-31 12:25:41 +0800

    Modify population.cpp

--- population.cpp
+++ population.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Speech culture century set course near.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
