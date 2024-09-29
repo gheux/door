9a19eff8757b60d6691a9de05f88728a456cf8bf
gheux <ialm0s9@outlook.com>
2024-09-29 13:39:42 +0800

    Modify provide.c

--- provide.c
+++ provide.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Send article argue religious town decision medical.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
