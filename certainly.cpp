948638568f44717a7fa9e52142ecdeef947be1e7
gheux <ialm0s9@outlook.com>
2024-10-26 13:48:21 +0800

    Modify certainly.cpp

--- certainly.cpp
+++ certainly.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Near far forget citizen.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
