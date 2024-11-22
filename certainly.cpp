229f056ed3c4d296ac2590e96aab101c05d0cc02
gheux <ialm0s9@outlook.com>
2024-11-22 13:44:16 +0800

    Modify certainly.cpp

--- certainly.cpp
+++ certainly.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Everybody suffer crime left heavy challenge player.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
