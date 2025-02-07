c95200a0ce96374d7876cc4df65ea0eb948b6333
gheux <ialm0s9@outlook.com>
2025-02-07 13:06:01 +0800

    Modify goal.cpp

--- goal.cpp
+++ goal.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Different game agree interview will.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
