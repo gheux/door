1dd4b2fcba1d53784c0eec3c3d7d188bd27bf8bf
gheux <ialm0s9@outlook.com>
2025-02-14 13:04:32 +0800

    Modify structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# After government factor scientist religious.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
