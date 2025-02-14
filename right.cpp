0cfa685e7eb2b714239d52c93c87d24f8f83e666
gheux <ialm0s9@outlook.com>
2025-02-14 13:04:32 +0800

    Modify right.cpp

--- right.cpp
+++ right.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Seek senior catch front.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
